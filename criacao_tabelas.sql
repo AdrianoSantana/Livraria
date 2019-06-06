e376e6789e6d7ec05acd0428eeec6124f4fe060f
AdrianoSantana <ans3@cin.ufpe.br>
2019-06-06 05:19:27 -0300

    Criacao de Tipos e Tabela Funcionando Perfeitamente

--- criacao_tabelas.sql
+++ criacao_tabelas.sql
@@ -1,22 +1,13 @@
-DROP TABLE TB_End;
 DROP TABLE TB_Curriculo;
-DROP TABLE TB_Pessoa;
-DROP TABLE TB_Tel_Pessoa;
 DROP TABLE TB_Cliente;
 DROP TABLE TB_Dependente;
 DROP TABLE TB_Editora;
 DROP TABLE TB_Empregado;
-DROP TABLE TB_Tel_Editora;
 DROP TABLE TB_Livro;
 DROP TABLE TB_Garantia;
 DROP TABLE TB_Compra;
 
 
-
-CREATE TABLE TB_End of TP_End (
-    PRIMARY KEY (cep)
-);
-/
 
 CREATE TABLE TB_Curriculo of TP_Curriculo (
 
@@ -25,24 +16,17 @@ CREATE TABLE TB_Curriculo of TP_Curriculo (
 );
 /
 
-CREATE TABLE TB_Pessoa of TP_Pessoa(
-
-    PRIMARY KEY (cpf)
-
-);
-/
-
-CREATE TABLE TB_Tel_Pessoa of TP_Tel_Pessoa (
-
-    FOREIGN KEY (ref_cpf) REFERENCES TB_Pessoa,
-    PRIMARY KEY(telefone_pessoa, ref_cpf)
-
-);
-/
-
 CREATE TABLE TB_Cliente of TP_Cliente (
 
-        PRIMARY KEY(cpf)
+        PRIMARY KEY(cpf_cliente)
+
+);
+/
+
+CREATE TABLE TB_Empregado of TP_Empregado (
+
+        PRIMARY KEY(cpf_empregado),
+        FOREIGN KEY(ref_chefe) REFERENCES TB_Empregado
 
 );
 /
@@ -50,29 +34,14 @@ CREATE TABLE TB_Cliente of TP_Cliente (
 CREATE TABLE TB_Dependente of TP_Dependente (
 
     FOREIGN KEY (ref_cpf_titular) REFERENCES TB_Cliente,
-    PRIMARY KEY(cpf_dependente, cpf)
+    PRIMARY KEY(cpf_dependente, cpf_titular)
 
 );
 /
 
-CREATE TABLE TB_Empregado of TP_Empregado (
-
-        PRIMARY KEY(cpf),
-        FOREIGN KEY(ref_chefe) REFERENCES TB_Empregado
-
-);
-/
 CREATE TABLE TB_Editora of TP_Editora (
 
     PRIMARY KEY(cnpj)
-
-);
-/
-CREATE TABLE TB_Tel_Editora of TP_Tel_Editora (
-
-    FOREIGN KEY(ref_cnpj) REFERENCES TB_Editora,
-    PRIMARY KEY(telefone_editora , ref_cnpj)
-
 
 );
 /
@@ -98,7 +67,7 @@ CREATE TABLE TB_Compra of TP_Compra (
     FOREIGN key(ref_cpf_empregado) REFERENCES TB_Empregado,
     FOREIGN KEY(ref_compra_cpf_cliente) REFERENCES TB_Cliente,
     FOREIGN key(ref_compra_id_livro) REFERENCES TB_Livro,
-    PRIMARY key(ref_compra_cpf_cliente,ref_cpf_empregado,ref_compra_id_livro,data_compra)
+    PRIMARY key(cpf_compra_cliente,cpf_compra_empregado,id_compra_livro,data_compra)
     
 
 );
--- criacao_tipos.sql
+++ criacao_tipos.sql
@@ -7,9 +7,15 @@ DROP TYPE TP_Empregado FORCE;
 DROP TYPE TP_End FORCE;
 DROP TYPE TP_Garantia FORCE;
 DROP TYPE TP_Livro FORCE;
-DROP TYPE TP_Tel_Editora FORCE;
+DROP TYPE TP_Tel FORCE;
 DROP TYPE TP_Pessoa FORCE;
-DROP TYPE TP_Tel_Pessoa FORCE;
+
+create or replace type TP_Tel as object(
+
+        telefone VARCHAR2(50)
+
+) FINAL;
+/
 
 
 CREATE OR REPLACE TYPE TP_End AS OBJECT (
@@ -31,32 +37,38 @@ create or replace type TP_Curriculo as object (
 
 create or replace type TP_Pessoa as object (
 
-        cpf VARCHAR2(9),
+
         nome Varchar2(50),
-        endereco  TP_End 
+        endereco  TP_End,
+        telefone_pessoa TP_Tel
 
 
 ) NOT FINAL;
 /
 
-create or replace type TP_Tel_Pessoa as object(
-
-        ref_cpf REF TP_Pessoa,
-        telefone_pessoa VARCHAR2(50)
-
-) FINAL;
-/
 
 create or replace type TP_Cliente UNDER TP_Pessoa (
 
+        cpf_cliente Varchar2(9),
         data_de_cadastro DATE
 
+)FINAL;
+
+/
+create or replace type TP_Empregado UNDER TP_Pessoa(
+
+            cpf_empregado VARCHAR2(9),
+            salario number,
+            data_de_contratacao DATE,
+            curriculo_emp TP_Curriculo,
+            ref_chefe REF TP_Empregado
 )FINAL;
 /
 
 create or replace type TP_Dependente as object(
 
         cpf_dependente VARCHAR2(9),
+        cpf_titular VARCHAR2(9),
         ref_cpf_titular REF TP_Cliente,
         nome_dependente VARCHAR2(50),
         idade_dependente number
@@ -65,32 +77,16 @@ create or replace type TP_Dependente as object(
 )FINAL;
 /
 
-
-create or replace type TP_Empregado UNDER TP_Pessoa(
-
-            salario number,
-            data_de_contratacao DATE,
-            curriculo_emp_id TP_Curriculo,
-            ref_chefe REF TP_Empregado
-)FINAL;
-/
-
 create or replace type TP_Editora as object (
 
         cnpj VARCHAR2(50),
-        nome VARCHAR2(50)
+        nome VARCHAR2(50),
+        telefone_editora TP_Tel
 
 
 )FINAL;
 /
 
-create or replace type TP_Tel_Editora as object(
-
-        ref_cnpj REF TP_Editora,
-        telefone_editora VARCHAR2(50)
-
-) FINAL;
-/
 
 create or replace type TP_Livro as object (
 
@@ -113,7 +109,9 @@ create or replace type TP_Garantia as object (
 /
 
 create or replace type TP_Compra as object (
-
+        cpf_compra_cliente Varchar2(9),
+        cpf_compra_empregado VARCHAR2(9),
+        id_compra_livro NUMBER,
         valor NUMBER,
         data_compra DATE,
         ref_cpf_empregado REF TP_Empregado,
