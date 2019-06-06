7e10ca9cba10fae6ff8279c4233d5c4e8c00d94a
AdrianoSantana <45883008+AdrianoSantana@users.noreply.github.com>
2019-06-06 09:42:33 -0300

    Começando Povoamento

---
+++ povoamento.sql
@@ -0,0 +1,30 @@
+
+--------------------Editora--------------------------------
+
+INSERT INTO TB_Editora 
+VALUES ('111000','Editora 1',TP_Tel('900001425'));
+
+INSERT INTO TB_Editora 
+VALUES ('222000','Editora 2',TP_Tel('981251425'));
+
+INSERT INTO TB_Editora 
+VALUES ('444000','Editora 2',TP_Tel('975892234'));
+
+INSERT INTO TB_Editora 
+VALUES ('555000','Editora 2',TP_Tel('981818181'));
+
+INSERT INTO TB_Editora 
+VALUES ('333000','Editora 2',TP_Tel('981818181'));
+
+-------------------------- Curriculo -------------------------------
+
+INSERT INTO TB_Curriculo 
+VALUES (011,'Livraria B','Médio');
+INSERT INTO TB_Curriculo  
+VALUES (125,'Livraria da esquina','Superior');
+INSERT INTO TB_Curriculo 
+ VALUES (023,'Livraria Amanhecer','Médio');
+INSERT INTO TB_Curriculo 
+ VALUES (314,'Livraria Verde','Fundamental');
+INSERT INTO TB_Curriculo 
+ VALUES (101,'Livraria Cultura','Superior');
