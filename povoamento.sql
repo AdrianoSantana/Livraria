776dd3c6bf70d808654c347ec69c48c601492e1a
AdrianoSantana <ans3@cin.ufpe.br>
2019-06-08 00:50:12 -0300

    Povoamento Completo, falta apenas as consultas

--- povoamento.sql
+++ povoamento.sql
@@ -1,7 +1,4 @@
-TB_Cliente;
-TB_Dependente;
-TB_Compra;
---------------------Editora--------------------------------
+-------------------------------------------------------Editora-------------------------------------------------
 
 INSERT INTO TB_Editora 
 VALUES ('111000','Editora 1',TP_Tel(TP_Fone('900001425')));
@@ -18,7 +15,7 @@ VALUES ('555000','Editora 2',TP_Tel(TP_Fone('981818181')));
 INSERT INTO TB_Editora 
 VALUES ('333000','Editora 2',TP_Tel(TP_Fone('981818181'), TP_Fone('991919191')));
 
--------------------------- Curriculo -------------------------------
+-------------------------------------------------- Curriculo -----------------------------------------------------
 
 INSERT INTO TB_Curriculo 
 VALUES (010,'Livraria B','Médio');
@@ -31,14 +28,14 @@ INSERT INTO TB_Curriculo
 INSERT INTO TB_Curriculo 
  VALUES (014,'Livraria Cultura','Superior');
 
- ---------------------- Livro ----------------------------
+ ----------------------------------------------- Livro ---------------------------------------------------------------
 INSERT INTO TB_Livro VALUES (0100,'Livro 1', (select ref (e) from TB_Editora e where e.cnpj = '111000'));
 INSERT INTO TB_Livro VALUES (0200,'Livro 2', (select ref (e) from TB_Editora e where e.cnpj = '222000'));
 INSERT INTO TB_Livro VALUES (0300,'Livro 3', (select ref (e) from TB_Editora e where e.cnpj = '333000'));
 INSERT INTO TB_Livro VALUES (0400,'Livro 4', (select ref (e) from TB_Editora e where e.cnpj = '444000'));
 INSERT INTO TB_Livro VALUES (0500,'Livro 5', (select ref (e) from TB_Editora e where e.cnpj = '555000'));
 
-------------------- Garantia ------------------------------
+------------------------------------------ Garantia --------------------------------------------------------------
 INSERT INTO TB_Garantia 
 VALUES (1000,12,to_date ('13/07/2019', 'dd/mm/yyyy'), (select ref(l) from TB_Livro l where l.id_livro = 0100) );
 
@@ -84,3 +81,78 @@ INSERT INTO TB_Empregado VALUES
 (select ref(c) from TB_Curriculo c where c.curriculo_id = '014'),
 (select ref(b) from TB_Empregado b where b.cpf_empregado = '000000000')
 );
+
+
+-------------------------------------------Cliente---------------------------------------------------------------
+INSERT INTO TB_Cliente VALUES 
+('Cliente 1', TP_End('12345','RUA F'),TP_Tel(TP_Fone ('900006666')),'555555555',to_date ('04/01/2012', 'dd/mm/yyyy'));
+
+INSERT INTO TB_Cliente VALUES 
+('Cliente 2', TP_End('54321','RUA G'),TP_Tel(TP_Fone ('900007777')),'666666666',to_date ('03/02/2013', 'dd/mm/yyyy'));
+
+INSERT INTO TB_Cliente VALUES 
+('Cliente 3', TP_End('12222','RUA H'),TP_Tel(TP_Fone ('900008888')),'777777777',to_date ('04/03/2014', 'dd/mm/yyyy'));
+
+INSERT INTO TB_Cliente VALUES 
+('Cliente 4', TP_End('13333','RUA I'),TP_Tel(TP_Fone ('900009999')),'888888888',to_date ('04/03/2014', 'dd/mm/yyyy'));
+
+INSERT INTO TB_Cliente VALUES 
+('Cliente 5', TP_End('14444','RUA J'),TP_Tel(TP_Fone ('911111111')),'999999999',to_date ('01/07/2019', 'dd/mm/yyyy'));
+
+------------------------------------------------- Dependente -------------------------------------------------------
+
+INSERT INTO TB_Dependente 
+VALUES ('000001111','999999999',(select ref(c) from TB_Cliente c where c.cpf_cliente = '999999999'),
+'Dependente 1',12);
+
+INSERT INTO TB_Dependente 
+VALUES ('000002222','888888888',(select ref(c) from TB_Cliente c where c.cpf_cliente = '888888888'),
+'Dependente 2',15);
+
+INSERT INTO TB_Dependente 
+VALUES ('000003333','777777777',(select ref(c) from TB_Cliente c where c.cpf_cliente = '777777777'),
+'Dependente 3',11);
+
+INSERT INTO TB_Dependente 
+VALUES ('000004444','666666666',(select ref(c) from TB_Cliente c where c.cpf_cliente = '666666666'),
+'Dependente 4',14);
+
+INSERT INTO TB_Dependente 
+VALUES ('000005555','555555555',(select ref(c) from TB_Cliente c where c.cpf_cliente = '555555555'),
+'Dependente 5',13);
+
+
+---------------------------------------- Compra ---------------------------------------------------------
+
+INSERT INTO TB_Compra 
+VALUES ('555555555','000000000',0300,35,to_date('04/01/2012','dd/mm/yyyy'),
+(select ref(e) from TB_Empregado e where e.cpf_empregado = '000000000'),
+(select ref(c) from TB_Cliente c where c.cpf_cliente = '555555555'),
+(select ref(l) from TB_Livro l where l.id_livro = 0300));
+
+INSERT INTO TB_Compra 
+VALUES ('666666666','000000000',0400,25,to_date('05/03/2012','dd/mm/yyyy'),
+(select ref(e) from TB_Empregado e where e.cpf_empregado = '000000000'),
+(select ref(c) from TB_Cliente c where c.cpf_cliente = '666666666'),
+(select ref(l) from TB_Livro l where l.id_livro = 0400));
+
+
+INSERT INTO TB_Compra 
+VALUES ('777777777','222222222',0200,30,to_date('04/04/2014','dd/mm/yyyy'),
+(select ref(e) from TB_Empregado e where e.cpf_empregado = '222222222'),
+(select ref(c) from TB_Cliente c where c.cpf_cliente = '777777777'),
+(select ref(l) from TB_Livro l where l.id_livro = 0200));
+
+
+INSERT INTO TB_Compra 
+VALUES ('888888888','111111111',0500,35,to_date('03/05/2017','dd/mm/yyyy'),
+(select ref(e) from TB_Empregado e where e.cpf_empregado = '111111111'),
+(select ref(c) from TB_Cliente c where c.cpf_cliente = '888888888'),
+(select ref(l) from TB_Livro l where l.id_livro = 0500));
+
+
+INSERT INTO TB_Compra 
+VALUES ('999999999','444444444',0100,35,to_date('01/07/2018','dd/mm/yyyy'),
+(select ref(e) from TB_Empregado e where e.cpf_empregado = '444444444'),
+(select ref(c) from TB_Cliente c where c.cpf_cliente = '999999999'),
+(select ref(l) from TB_Livro l where l.id_livro = 0100));
